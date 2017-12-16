a2a9b5bcf8a277bb3ae7f3b16e36edb78f338e84
Advait Ambeskar <ambeskaradvait@gmail.com>
2017-12-16 00:56:02 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            int randomNumber = 5;
+            Console.WriteLine("The Random Number is {0}", randomNumber);
+            string yourName;
+            Console.WriteLine("What is your name?");
+            yourName = Console.ReadLine();
+            Console.WriteLine("Hello {0}. It is a pleasure to meet you.", yourName);
+            int yourAge;
+            Console.WriteLine("Could I please ask you for your age?");
+            yourAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Oh! So you are {0} years old then!", yourAge);
+            //The above code is just random examples of input and output
+            /*
+             * This infact is an example of just how commenting works.
+             */
+            var randomInformation = true;
+            //Console.WriteLine(randomInformation);
+            const double Pi = 3.14;
+            //Console.WriteLine("The value of Pi is {0}", Pi);
+            if (Pi * yourAge > 3.1 * 21)
+            {
+                Console.WriteLine("You are old enough to drink!");
+                Console.WriteLine("So, how would you like to celebrate? Wait... before that.. I need to know \nHow much marks did you get?");
+                double marks = Convert.ToDouble(Console.ReadLine());
+                if (marks > 50)
+                {
+                    Console.Write("Congrats!");
+                    if (marks == 100)
+                    {
+                        Console.Write(" You have a perfect score!\n");
+                    }
+                    else if (marks > 90)
+                    {
+                        Console.Write(" That is a high score! Wow!");
+                    }
+                    Console.WriteLine("Here is your drink!");
+                }
+                else
+                {
+                    Console.WriteLine("No Drinks for you!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("UNDERAGE!! FELONY!!");
+            }
+        }
+    }
+}
